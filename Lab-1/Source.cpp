0a4603286226a202a7fbc0760d22c61720017a8b
Denis <denis.pptx@gmail.com>
2023-09-25 00:14:29 +0300

    added combo boxes

--- Lab-1/Source.cpp
+++ Lab-1/Source.cpp
@@ -1,15 +1,95 @@
 #include <windows.h>
 
+#define ID_COMBOBOX1 1
+#define ID_COMBOBOX2 2
+
+HWND hComboBox1;
+HWND hComboBox2;
+
+int selected_base_combobox1 = 1;
+int selected_base_combobox2 = 0;
+
+
+int combobox_index_to_base(int index) {
+    if (index == 0)
+        return 2;
+
+    if (index == 1)
+        return 10;
+
+    if (index == 2)
+        return 16;
+}
+
+void MainWindowAddComboBoxes(HWND hWnd) {
+    hComboBox1 = CreateWindow(
+        L"COMBOBOX", L"",
+        WS_CHILD | WS_VISIBLE | CBS_DROPDOWNLIST,
+        50, 50, 100, 200,
+        hWnd,
+        (HMENU)ID_COMBOBOX1,
+        NULL,
+        NULL
+    );
+    SendMessage(hComboBox1, CB_ADDSTRING, 0, (LPARAM)L"2");
+    SendMessage(hComboBox1, CB_ADDSTRING, 0, (LPARAM)L"10");
+    SendMessage(hComboBox1, CB_ADDSTRING, 0, (LPARAM)L"16");
+    SendMessage(hComboBox1, CB_SETCURSEL, selected_base_combobox1, 0);
+
+
+    hComboBox2 = CreateWindow(
+        L"COMBOBOX", L"",
+        WS_CHILD | WS_VISIBLE | CBS_DROPDOWNLIST,
+        50, 100, 100, 200,
+        hWnd,
+        (HMENU)ID_COMBOBOX2,
+        NULL,
+        NULL
+    );
+    SendMessage(hComboBox2, CB_ADDSTRING, 0, (LPARAM)L"2");
+    SendMessage(hComboBox2, CB_ADDSTRING, 0, (LPARAM)L"10");
+    SendMessage(hComboBox2, CB_ADDSTRING, 0, (LPARAM)L"16");
+    SendMessage(hComboBox2, CB_SETCURSEL, selected_base_combobox2, 0);
+}
+
 
 // Функция обработки сообщений
-LRESULT CALLBACK MainWindowProcedure(HWND hw, UINT msg, WPARAM wp, LPARAM lp) {
-	switch (msg) {
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
-	}
+LRESULT CALLBACK MainWindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg)
+    {
+    case WM_CREATE:
+    {
+        MainWindowAddComboBoxes(hWnd);
+        break;
+    }
 
-	return DefWindowProc(hw, msg, wp, lp);
+    case WM_COMMAND:
+    {
+        switch (LOWORD(wParam))
+        {
+        case ID_COMBOBOX1:
+            if (HIWORD(wParam) == CBN_SELCHANGE)
+                selected_base_combobox1 = combobox_index_to_base(SendMessage(hComboBox1, CB_GETCURSEL, 0, 0));
+            
+            break;
+
+        case ID_COMBOBOX2:
+            if (HIWORD(wParam) == CBN_SELCHANGE)
+                selected_base_combobox2 = combobox_index_to_base(SendMessage(hComboBox2, CB_GETCURSEL, 0, 0));
+            
+            break;
+        }
+
+        break;
+    }
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hWnd, msg, wParam, lParam);
+    }
 }
 
 
@@ -31,7 +111,13 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int ss) {
 		return EXIT_FAILURE;
 
 	// Создание главного окна
-	HWND hw = CreateWindow(L"MainWindowClass", L"Конвертер", WS_OVERLAPPEDWINDOW, 0, 0, 640, 480, NULL, NULL, hInst, NULL);
+	HWND hw = CreateWindow(
+		L"MainWindowClass", L"Конвертер", 
+		WS_OVERLAPPEDWINDOW, 
+		0, 0, 640, 480, 
+		NULL, NULL, hInst, NULL
+	);
+
 	if (!hw)
 		return EXIT_FAILURE;
 
