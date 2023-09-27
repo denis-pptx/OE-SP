0db2c9aebb567b1c489f515def1d733485c7a9e5
Denis-HP <denis.pptx@gmail.com>
2023-09-27 11:38:45 +0300

    display city in window title

--- Lab-2/Source.cpp
+++ Lab-2/Source.cpp
@@ -4,12 +4,40 @@
 #include <string>
 #include <string>
 #include <ctime>
+#include <map>
 using namespace std;
 
 
 
 int utc_offset = 3;
 wstring BASE_TITLE = L"Clock | UTC";
+
+map<int, wstring> TIME_ZONE_MAP = {
+       {-11, L"Midway Island"},
+       {-10, L"Honolulu"},
+       {-9, L"Anchorage"},
+       {-8, L"Los Angeles"},
+       {-7, L"Denver"},
+       {-6, L"Chicago"},
+       {-5, L"New York"},
+       {-4, L"Caracas"},
+       {-3, L"Buenos Aires"},
+       {-2, L"South Georgia"},
+       {-1, L"Azores"},
+       {0, L"London"},
+       {1, L"Paris"},
+       {2, L"Athens"},
+       {3, L"Minsk"},
+       {4, L"Kabul"},
+       {5, L"Islamabad"},
+       {6, L"Almaty"},
+       {7, L"Bangkok"},
+       {8, L"Beijing"},
+       {9, L"Tokyo"},
+       {10, L"Sydney"},
+       {11, L"Solomon Islands"},
+       {12, L"Suva"}
+};
 
 LRESULT CALLBACK MainWindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
@@ -163,6 +191,7 @@ void DisplayTimeZone(HWND hWnd) {
     else if (utc_offset > 0) 
         new_title += L"+" + to_wstring(utc_offset);
    
+    new_title += L" | " + TIME_ZONE_MAP[utc_offset];
     SetWindowText(hWnd, new_title.c_str());
 }
 
