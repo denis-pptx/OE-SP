742aac4a6797a066b06511b1a722deea29372195
Denis-PC <denis.pptx@gmail.com>
2023-11-21 21:27:43 +0300

    modified client app for many users

--- Lab-7/Client/Client.cpp
+++ Lab-7/Client/Client.cpp
@@ -1,114 +1,201 @@
-#include <windows.h>
+#include <Windows.h>
+#include <string>
+#include <thread>
+#include <iostream> 
+#pragma comment(lib, "ws2_32.lib")
 
+using namespace std;
 
-#pragma comment(lib, "ws2_32.lib") // Линковка с библиотекой для работы с Winsock
+#define DEFAULT_PORT 12345
+#define ID_BUTTON_SEND 1
 
-#define IDC_EDIT   101
-#define IDC_BUTTON 102
-
-HWND hwndEdit;
-HWND hwndButton;
+HWND hNameEdit, hMessageEdit, hListBox;
 SOCKET clientSocket;
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+void HandleMessages() {
+    while (true) {
+        char buffer[1000];
+        int bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+        if (bytesRead <= 0) {
+            // Обработка ошибки при чтении сообщений от сервера или отключения
+            cerr << "Error reading messages or server disconnected." << endl;
+            closesocket(clientSocket);
+            return;
+        }
 
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    // Инициализация Winsock (аналогично серверу)
-    WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        MessageBox(NULL, L"Ошибка при инициализации Winsock.", L"Ошибка", MB_OK | MB_ICONERROR);
-        return 1;
+        buffer[bytesRead] = '\0';
+        SendMessageA(hListBox, LB_ADDSTRING, 0, reinterpret_cast<LPARAM>(buffer));
     }
-
-    // Создание сокета
-    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket == INVALID_SOCKET) {
-        MessageBox(NULL, L"Ошибка при создании сокета.", L"Ошибка", MB_OK | MB_ICONERROR);
-        WSACleanup();
-        return 1;
-    }
-
-    // Запрос на подключение к серверу
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1"); // IP-адрес сервера
-    serverAddress.sin_port = htons(12345); // Порт сервера
-
-    if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
-        MessageBox(NULL, L"Ошибка при подключении к серверу.", L"Ошибка", MB_OK | MB_ICONERROR);
-        closesocket(clientSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    // Создание главного окна
-    WNDCLASSEX wc;
-    HWND hwnd;
-    MSG Msg;
-
-    wc.cbSize = sizeof(WNDCLASSEX);
-    wc.style = 0;
-    wc.lpfnWndProc = WndProc;
-    wc.cbClsExtra = 0;
-    wc.cbWndExtra = 0;
-    wc.hInstance = hInstance;
-    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wc.lpszMenuName = NULL;
-    wc.lpszClassName = L"ChatApp";
-    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-
-    if (!RegisterClassEx(&wc)) {
-        MessageBox(NULL, L"Ошибка при регистрации окна!", L"Ошибка!", MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    }
-
-    hwnd = CreateWindowEx(WS_EX_CLIENTEDGE, L"ChatApp", L"Chat Client", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 400, 300, NULL, NULL, hInstance, NULL);
-
-    if (hwnd == NULL) {
-        MessageBox(NULL, L"Ошибка при создании окна!", L"Ошибка!", MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    }
-
-    ShowWindow(hwnd, nCmdShow);
-    UpdateWindow(hwnd);
-
-    // Основной цикл сообщений
-    while (GetMessage(&Msg, NULL, 0, 0) > 0) {
-        TranslateMessage(&Msg);
-        DispatchMessage(&Msg);
-    }
-
-    closesocket(clientSocket); // Закрытие сокета клиента
-    WSACleanup(); // Очистка Winsock
-
-    return Msg.wParam;
 }
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-    switch (message) {
+void ConnectToServer(const char* serverIP) {
+    WSADATA wsaData;
+    // Инициализация библиотеки Winsock
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        cerr << "Failed to initialize Winsock." << endl;
+        exit(1);
+    }
+
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        cerr << "Failed to create socket." << endl;
+        WSACleanup();
+        exit(1);
+    }
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(DEFAULT_PORT);
+
+    // Преобразование IP-адреса из строки
+    serverAddr.sin_addr.s_addr = inet_addr(serverIP);
+
+    if (serverAddr.sin_addr.s_addr == INADDR_NONE) {
+        cerr << "Invalid server IP address." << endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        exit(1);
+    }
+
+    // Подключение к серверу
+    if (connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        cerr << "Failed to connect to the server." << endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        MessageBox(NULL, L"Failed to connect to the server.", L"Error", MB_OK | MB_ICONERROR);
+        exit(1);
+    }
+
+    // Запуск потока для приема сообщений от сервера
+    thread(HandleMessages).detach();
+}
+
+
+void SendButtonClicked() {
+    char name[100], message[500];
+    GetWindowTextA(hNameEdit, name, sizeof(name) / sizeof(name[0]));
+    GetWindowTextA(hMessageEdit, message, sizeof(message) / sizeof(message[0]));
+
+    string fullMessage = name;
+    fullMessage += ": ";
+    fullMessage += message;
+
+    // Отправка сообщения на сервер
+    if (send(clientSocket, fullMessage.c_str(), fullMessage.size(), 0) == SOCKET_ERROR) {
+        cerr << "Failed to send message to the server." << endl;
+        // Дополнительные действия по обработке ошибки, если необходимо
+    }
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
     case WM_CREATE:
-        hwndEdit = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"", WS_CHILD | WS_VISIBLE | ES_MULTILINE | ES_AUTOVSCROLL | WS_VSCROLL, 10, 10, 350, 200, hwnd, (HMENU)IDC_EDIT, GetModuleHandle(NULL), NULL);
-        hwndButton = CreateWindow(L"BUTTON", L"Отправить", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 10, 220, 50, 25, hwnd, (HMENU)IDC_BUTTON, GetModuleHandle(NULL), NULL);
+
+        CreateWindowEx(
+            0, L"STATIC", L"Name:",
+            WS_CHILD | WS_VISIBLE,
+            10, 10, 70, 25,
+            hwnd, NULL, NULL, NULL
+        );
+        hNameEdit = CreateWindowEx(
+            0, L"EDIT", NULL,
+            WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOHSCROLL,
+            80, 10, 120, 25,
+            hwnd, NULL, NULL, NULL
+        );
+        SendMessage(hNameEdit, EM_LIMITTEXT, 10, 0);
+
+        CreateWindowEx(
+            0, L"STATIC", L"Message:",
+            WS_CHILD | WS_VISIBLE,
+            10, 40, 70, 25,
+            hwnd, NULL, NULL, NULL
+        );
+        hMessageEdit = CreateWindowEx(
+            0, L"EDIT", NULL, 
+            WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOHSCROLL,
+            80, 40, 250, 25, 
+            hwnd, NULL, NULL, NULL
+        );
+        SendMessage(hMessageEdit, EM_LIMITTEXT, 40, 0);
+        CreateWindowEx(
+            0, L"BUTTON", L"Отправить", 
+            WS_CHILD | WS_VISIBLE,
+            340, 40, 80, 25,
+            hwnd, reinterpret_cast<HMENU>(ID_BUTTON_SEND), NULL, NULL);
+
+
+        hListBox = CreateWindowEx(
+            0, L"LISTBOX", NULL,
+            WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_AUTOVSCROLL,
+            10, 70, 410, 200,
+            hwnd, NULL, NULL, NULL
+        );
+
+
+        
+
+        // Подключение к серверу
+        ConnectToServer("127.0.0.1");
         break;
+
     case WM_COMMAND:
-        if (LOWORD(wParam) == IDC_BUTTON) {
-            char buffer[1024];
-            GetWindowTextA(hwndEdit, buffer, sizeof(buffer));
-
-            // Отправка буфера на сервер
-            send(clientSocket, buffer, strlen(buffer) * sizeof(char), 0);
-
-            SetWindowText(hwndEdit, L""); // Очистка текстового поля
+        if (LOWORD(wParam) == 1 && HIWORD(wParam) == BN_CLICKED) {
+            SendButtonClicked();
         }
         break;
+
     case WM_DESTROY:
+        // Завершение работы с сокетом и Winsock
+        closesocket(clientSocket);
+        WSACleanup();
         PostQuitMessage(0);
         break;
+
     default:
-        return DefWindowProc(hwnd, message, wParam, lParam);
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
 
     return 0;
+}
+
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR cmdline, int ss) {
+
+    // Создание класса главного окна 
+    WNDCLASS wc;
+    wc.style = NULL;
+    wc.lpfnWndProc = WindowProc;
+    wc.cbClsExtra = NULL;
+    wc.cbWndExtra = NULL;
+    wc.hInstance = hInst;
+    wc.hIcon = NULL;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW);
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = L"MainWindowClass";
+    if (!RegisterClass(&wc))
+        return EXIT_FAILURE;
+
+    // Создание главного окна
+    HWND hWnd = CreateWindow(
+        L"MainWindowClass", L"KonchikMessenger",
+        WS_OVERLAPPEDWINDOW,
+        0, 0, 450, 450,
+        NULL, NULL, hInst, NULL
+    );
+
+    if (!hWnd)
+        return EXIT_FAILURE;
+
+    ShowWindow(hWnd, ss);
+
+    // Цикл обработки сообщений
+    MSG msg;
+    while (GetMessageW(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
 }
